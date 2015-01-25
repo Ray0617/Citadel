8b03ce335653db0c44050fcb8ce3596ad633502f
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-01-25 13:35:27 -0500

    first version that works
    Change-Id: I9f737ccaa555915e2a43fee2b6a8f020347e4a0e

--- src/CarDispatcher.h
+++ src/CarDispatcher.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <deque>
 #include <stdio.h>
 #include "TransportationTypes.h"
 
@@ -11,195 +12,171 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <math.h>
+#include <assert.h>
 using namespace std;
+
+//#define _DEBUG
+
+struct PassengerInfo {
+  PassengerRequest request;
+  int status; // 0: wait; 1: moving & fulfill
+};
 
 class CarDispatcher { 
 
 public:
 
-  // debug
-  void outputRoadInfo(const RoadInfo& info, ofstream& output) {
-    output << "road: " << info.road_id << "\t";
-    output << info.src_intersection_id << "\t";
-    output << info.dst_intersection_id << "\t";
-    output << info.weight << endl;
-  }
-
-  void outputPassengerRequest(const PassengerRequest& request, ofstream& output) {
-    output << "req: " << request.passenger_id << "\t" << request.src_intersection_id << "\t" << request.dst_intersection_id << endl;
-  }
-  
-  void outputIntersectionInfo(const IntersectionInfo& info, ofstream& output) {
-    output << "intersect: " << info.intersection_id << "\t" << info.x_location << "\t" << info.y_location << endl;
-  }
-  
-  void outputCarCtl(const CarCtl& car, ofstream& output) {
-    output << "car: " << car.car_id << "\t" << car.intersection_id << "\t" << car.passenger_id << endl;
-  }
-  
-  void outputRoads() {
-    ofstream output("roads.txt");
-	for (unsigned i = 0; i < roads.size(); i++) {
-	  outputRoadInfo(roads[i], output);
-	}
-	output.close();
-  }
-
-  void outputIntersections() {
-    ofstream output("intersections.txt");
-	for (unsigned i = 0; i < intersections.size(); i++) {
-	  outputIntersectionInfo(intersections[i], output);
-	}
-	output.close();
-  }
-
   CarDispatcher(std::vector<IntersectionInfo> _intersections, std::vector<RoadInfo> _roads)
   : intersections(_intersections)
   , roads(_roads)
   {
-  
-    //outputRoads();
-    //outputIntersections();
-    
-	// init shortest path by intersections and roads
-	vector<vector<int> > distance;
-    shortestPath.resize(intersections.size());
-    distance.resize(intersections.size());
+	shortest_node.resize(intersections.size());
+	distance.resize(intersections.size());
     for (unsigned i = 0; i < intersections.size(); i++) {
-	  shortestPath[i].resize(intersections.size());
-      distance[i].resize(intersections.size());
+  	  shortest_node[i].resize(intersections.size());
+	  distance[i].resize(intersections.size());
       for (unsigned j = 0; j < intersections.size(); j++) {
-  	    distance[i][j] = INT_MAX;
+	    shortest_node[i][j] = -1;
+		distance[i][j] = INT_MAX;
 	  }
-	  distance[i][i] = 0;
-	}
-	vector<pair<int,int> > dirty;
-    for (unsigned i = 0; i < roads.size(); i++) {
-	  distance[roads[i].src_intersection_id][roads[i].dst_intersection_id] = roads[i].weight;
-	  dirty.push_back(make_pair(roads[i].src_intersection_id, roads[i].dst_intersection_id));
-	  shortestPath[roads[i].src_intersection_id][roads[i].dst_intersection_id].insert(roads[i].dst_intersection_id);
-	}
-	while (!dirty.empty()) {
-	  pair<int,int> road = dirty.back();
-	  dirty.pop_back();
-      for (unsigned i = 0; i < intersections.size(); i++) {
-	    if (i == (unsigned)road.first || distance[i][road.first] == INT_MAX)
-		  continue;
-	    int dist = distance[i][road.first] + distance[road.first][road.second];
-	    if (dist < distance[i][road.second]) {
-		  distance[i][road.second] = dist;
-		  dirty.push_back(make_pair(i, road.second));
-		  shortestPath[i][road.second].clear();
-		  shortestPath[i][road.second].insert(road.first);
-		} else if (dist == distance[i][road.second]) {
-		  shortestPath[i][road.second].insert(road.first);
-		}
-	  }
+	  assert((unsigned)intersections[i].intersection_id == i);
 	}
 	
-	if (0) { // test shortestPath
-	  ofstream output("shortestPath.txt");
-	  for (unsigned i = 0; i < intersections.size(); i++) {
-	    for (unsigned j = 0; j < intersections.size(); j++) {
-	      if (distance[i][j] == INT_MAX)
-	  	  output << "---- ";
-	      else {
-	  	  output << distance[i][j];
-	  	  output << "(";
-	  	  for (set<int>::iterator it = shortestPath[i][j].begin(); it != shortestPath[i][j].end(); it++) {
-	  	    output << *it;
-	  	  }
-	  	  output << ") ";
-	  	}
-	    }
-	    output << endl;
-	  }
-	  output.close();
-	}
-	
-    center_x = 0;
-	center_y = 0;
-    for (unsigned i = 0; i < intersections.size(); i++) {
-		center_x += intersections[i].x_location;
-		center_y += intersections[i].y_location;
-	}
-	center_x /= intersections.size();
-	center_y /= intersections.size();
+	roads_in_intersection.resize(intersections.size());
+	for (unsigned i = 0; i < roads.size(); i++) {
+	  roads_in_intersection[roads[i].src_intersection_id].push_back(roads[i]);
+	}	
+  }
 
-    ofstream output("debug.txt");	// clean debug file
-	output.close();
+  double directDistance(int id1, int id2) {
+    int dx = intersections[id1].x_location - intersections[id2].x_location;
+    int dy = intersections[id1].y_location - intersections[id2].y_location;
+    return sqrt(dx * dx + dy * dy);
+  }
+  
+  void init(int from) {
+    vector<bool> dirty(intersections.size());
+    dirty[from] = true;
+    distance[from][from] = 0;
+	shortest_node[from][from] = 0;
+    bool found = true;
+    while (found) {
+    found = false;
+    for (unsigned i = 0; i < intersections.size(); i++) {
+      if (dirty[i]) {
+    	for (unsigned r = 0; r < roads_in_intersection[i].size(); r++) {
+    	  if (distance[from][i] == INT_MAX || 
+    		roads_in_intersection[i][r].weight + distance[from][i] < 
+    		distance[from][roads_in_intersection[i][r].dst_intersection_id]) {
+    		  distance[from][roads_in_intersection[i][r].dst_intersection_id] = 
+    		    roads_in_intersection[i][r].weight + distance[from][i];
+    		  shortest_node[from][roads_in_intersection[i][r].dst_intersection_id] = 
+    		    roads_in_intersection[i][r].src_intersection_id;
+    		  dirty[roads_in_intersection[i][r].dst_intersection_id] = true;
+    		  found = true;
+			}
+    	  }
+    	}
+    	dirty[i] = false;
+      }
+    }
+#ifdef _DEBUG
+    printf("init %d\n", from);
+    for (unsigned i = 0; i < intersections.size(); i++) {
+	  printf("to %d dist %d thr node %d\n", i, distance[from][i], shortest_node[from][i]);
+	}
+#endif			  
+  }
+  
+  int findNextNode(int from, int to) {
+    if (shortest_node[from][from] != 0) {
+	  init(from);
+	}
+	int prev = shortest_node[from][to];
+	if (prev == from)
+	  return to;
+	return findNextNode(from, prev);
+  }
+  
+  int findDistance(int src, int dst) {
+    if (shortest_node[src][src] != 0) {
+      init(src);
+	}
+	return distance[src][dst];
   }
 
   void onTurn(std::vector<CarCtl> & cars_at_intersections, 
-              const std::vector<PassengerRequest> & _passenger_requests) {
-	
-	// version 0. 
-	//testSimpleMap(cars_at_intersections, _passenger_requests);
-	//return;
-	
-	
-	int size = passenger_requests.size();
-	passenger_requests.resize(size + _passenger_requests.size());
-	copy(_passenger_requests.begin(), _passenger_requests.end(), passenger_requests.begin() + size);	
-	
-	// for each idle car, decide if need to forward to center
-	// version 1. find nearest passenger and go
-	vector<int> load(intersections.size());
-	for (unsigned i = 0; i < cars_at_intersections.size(); i++) {
-	  // check if any passenger at the same position
-	  for (unsigned p = 0; p < passenger_requests.size(); p++) {
-	    if (passenger_requests[p].src_intersection_id == cars_at_intersections[i].intersection_id) {
-		  cars_at_intersections[i].passenger_id = passenger_requests[p].passenger_id;
-		  set<int>& paths = shortestPath[passenger_requests[p].src_intersection_id][passenger_requests[p].dst_intersection_id];
-		  int minLoad = INT_MAX;
-		  int minNode = -1;
-		  for (set<int>::iterator it = paths.begin(); it != paths.end(); it++) {
-		    if (load[*it] < minLoad) {
-			  minLoad = load[*it];
-			  minNode = *it;
-			}
-		  }
-		  cars_at_intersections[i].intersection_id = minNode;
-		  load[minNode]++;
-		  passenger_requests[p] = passenger_requests[passenger_requests.size()-1];
-		  passenger_requests.pop_back();
-		  break;
-		}
-      }
-	}
-  }
-  void testSimpleMap(std::vector<CarCtl> & cars_at_intersections, 
               const std::vector<PassengerRequest> & passenger_requests) {
-    static int turn = 0;
-    ofstream output("debug.txt", ios::app);
-	output << "turn " << turn++ << ": " << endl;
-	for (unsigned int i = 0; i < cars_at_intersections.size(); i++)
-	  outputCarCtl(cars_at_intersections[i], output);
-	for (unsigned int i = 0; i < passenger_requests.size(); i++)
-	  outputPassengerRequest(passenger_requests[i], output);
-	output.close();
+#ifdef _DEBUG
+	static int turn = 0;
+	printf("turn %d:\n", turn++);
+#endif
+	
+	for (unsigned i = 0; i < passenger_requests.size(); i++) {
+	  PassengerInfo info = {passenger_requests[i], 0};
+	  passenger_info[passenger_requests[i].passenger_id] = info;
+	}
+#ifdef _DEBUG
+    printf("passenger info:\n");
+	for (map<int, PassengerInfo>::iterator it = passenger_info.begin(); it != passenger_info.end(); it++) {
+	  printf("%d: (%d => %d) %d\n", it->second.request.passenger_id, 
+	    it->second.request.src_intersection_id, it->second.request.dst_intersection_id, it->second.status);
+	}
+	// print all passenger info
+#endif
 
-	if (turn == 1) {
-	  cars_at_intersections[0].intersection_id = 0;
-	} else if (turn == 680) {
-	  cars_at_intersections[0].intersection_id = 2;
-	  cars_at_intersections[0].passenger_id = 0;
-	} else if (turn == 1360) {
-	  cars_at_intersections[0].intersection_id = 1;
-	  cars_at_intersections[0].passenger_id = 1;
-	} else if (turn == 1880) {
-	  cars_at_intersections[0].intersection_id = 3;
-	  cars_at_intersections[0].passenger_id = 1;
-	} else if (turn == 2104) {
-	  cars_at_intersections[0].intersection_id = 1;
-	  cars_at_intersections[0].passenger_id = 3;
-	} else if (turn == 2328) {
-	  cars_at_intersections[0].intersection_id = 0;
-	} else if (turn == 3007) {
-	  cars_at_intersections[0].intersection_id = 2;
-	} else if (turn == 3687) {
-	  cars_at_intersections[0].intersection_id = 1;
-	  cars_at_intersections[0].passenger_id = 2;
+    // might be optimized by checking passenger and decide which car would serve
+	
+#ifdef _DEBUG
+    printf("car info:\n");
+    for (unsigned i = 0; i < cars_at_intersections.size(); i++) {
+	  printf("%d %d %d\n", cars_at_intersections[i].car_id, cars_at_intersections[i].intersection_id, 
+	    cars_at_intersections[i].passenger_id);
+	}
+#endif	
+	// for each idle car, decide if need to find nearest passenger or move to place without cars
+	for (unsigned i = 0; i < cars_at_intersections.size(); i++) {
+	  if (cars_at_intersections[i].passenger_id >= 0) { // with passenger; just find a nearest route to dst
+	    cars_at_intersections[i].intersection_id = findNextNode(
+		  cars_at_intersections[i].intersection_id, 
+		  passenger_info[cars_at_intersections[i].passenger_id].request.dst_intersection_id);
+	  } else { // no passenger; try find a nearest passenger 
+	    int nearestDistance = INT_MAX;
+	    int nearestPassenger = -1;
+	    for (map<int, PassengerInfo>::iterator it = passenger_info.begin(); it != passenger_info.end(); it++) {
+		  if (it->second.status)
+		    continue;
+	      int dist = findDistance(cars_at_intersections[i].intersection_id, it->second.request.src_intersection_id);
+#ifdef _DEBUG
+          printf("distance from passenger %d is %d\n", it->first, dist);
+#endif		
+		  if (dist < nearestDistance) { // might be optimized by checking if passenger's dst has another passenger 
+		    nearestDistance = dist;
+		    nearestPassenger = it->first;
+		  }
+	    }
+#ifdef _DEBUG
+        printf("nearest passenger %d at distance %d\n", nearestPassenger, nearestDistance);
+#endif		
+	    if (nearestPassenger >= 0) {
+	      if (nearestDistance == 0) { // just pickup the passenger, and toward to dst
+		    cars_at_intersections[i].passenger_id = passenger_info[nearestPassenger].request.passenger_id;
+		    passenger_info[nearestPassenger].status = 1;
+			cars_at_intersections[i].intersection_id = findNextNode(cars_at_intersections[i].intersection_id, 
+			  passenger_info[nearestPassenger].request.dst_intersection_id);
+		  } else {  // toward to the passenger
+			cars_at_intersections[i].intersection_id = findNextNode(cars_at_intersections[i].intersection_id, 
+			  passenger_info[nearestPassenger].request.src_intersection_id);
+#ifdef _DEBUG
+            printf("findNextNode(%d,%d)=%d\n", cars_at_intersections[i].intersection_id, passenger_info[nearestPassenger].request.src_intersection_id, cars_at_intersections[i].intersection_id);
+#endif		
+		  }
+		} else { // no passenger at all, do nothing
+		  // might be optimized by checking the distribution of cars
+		  continue;
+		}
+	  }
 	}
   }
   
@@ -207,10 +184,10 @@ private:
   vector<IntersectionInfo> intersections;
   vector<RoadInfo> roads;
   map<int, pair<CarCtl, int> > cars;	// time remain to get destination
-  vector<PassengerRequest> passenger_requests;
-  vector<vector<set<int> > > shortestPath;	//[src][dst][n]: might have multiple shorest path (n)
-  int center_x;
-  int center_y;
+  map<int, PassengerInfo> passenger_info;
+  vector<vector<int> > shortest_node;
+  vector<vector<int> > distance;
+  vector<vector<RoadInfo> > roads_in_intersection;
 };
 
 
